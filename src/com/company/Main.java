e0d8c1deaee85a23ec423c63f880c448cfbbd51e
Pasek Dawid <idekpas95@gmail.com>
2021-01-05 09:28:15 +0100

    review

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,39 @@
 package com.company;
 
+import java.util.Scanner;
 public class Main {
+    public static int[][] tab = new int[9][9];
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner gameScan = new Scanner(System.in);
+        Sudoku sudoku = new Sudoku(gameScan);
+        if (!sudoku.startup()) {
+            return;
+        } else {
+            while (true) {
+                System.out.println("\nKtórą Czynność chcesz wykonać?\n1: Pokazać plansze 2: wypełnić/zmienić pole 3: Zmienić poziom\n4: Sprawdzić wynik 5: Wyjść z programu");
+                int x = gameScan.nextInt();
+
+                switch (x) {
+                    case 1:
+                        sudoku.showSudoku(tab);
+                        break;
+
+                    case 2:
+                        sudoku.fillSudoku(tab);
+                        break;
+                    case 3:
+
+                        sudoku.levelSudoku(tab);
+                        break;
+                    case 4:
+                        sudoku.validate(tab);
+                        break;
+                    case 5:
+                        System.exit(0);
+                }
+            }
+
+        }
     }
 }
